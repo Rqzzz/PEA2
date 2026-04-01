55b3bafe80d624ac16b21f0e822dab9a4592a879
Rqzzz <u1391351@utah.edu>
2026-03-31 18:21:00 -0600

    godotredo

---
+++ coin-a-3-redo/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody3D
+
+@export var speed = 5.0
+
+func _physics_process(_delta):
+
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	
+
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+	
+
+	move_and_slide()
